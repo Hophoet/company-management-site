593c7f387fea70e4aa392ff824cba37c8d840e0d
hophoet <hophoet@gmail.com>
2021-03-25 00:47:20 +0000

    (redux) - set the redux provider component

--- src/index.js
+++ src/index.js
@@ -4,10 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import "@coreui/coreui/scss/coreui.scss";
+
+import { Provider } from 'react-redux'
+import store from './store'
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
