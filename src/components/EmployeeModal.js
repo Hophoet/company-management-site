0de0aa8c4342c67d3e7abacbfef6570343b9fd55
hophoet <hophoet@gmail.com>
2021-03-26 17:19:43 +0000

    (components) - add employee add handle modal component

---
+++ src/components/EmployeeModal.js
@@ -0,0 +1,156 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import { motion } from 'framer-motion';
+import '../styles/EmployeeModal.css';
+
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CCardFooter,
+  CCol,
+  CContainer,
+  CForm,
+  CInput,
+  CInputGroup,
+  CInputGroupPrepend,
+  CInputGroupText,
+  CRow,
+  CIcon
+} from '@coreui/react'
+
+
+export default class AddPictureModal extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            file:null,
+            fileError:null,
+            fileURL:'',
+            employeeUsername:'',
+            employeePassword:''
+       }
+        this.employeeToUpdate = this.props.employeeToUpdate
+        this.fileTypes = ['image/png', 'image/jpeg'];
+    }
+
+    _save = () => {
+        if(this._formIsValid()){
+            //check if it the update
+            this._addNewEmployee()
+        }
+
+    }
+    
+    _addNewEmployee = () => {
+        alert('ready to add the new employee')
+    }
+
+
+    _formIsValid = () => {
+        if(this.state.employeePassword && this.state.employeePassword && this.state.file){
+            return true
+        }
+        else if(!this.state.employeeUsername){
+            alert('Enter the employee name');
+        }
+        else if(!this.state.employeePassword){
+            alert('Enter the employee password');
+        }
+        else if(!this.state.file){
+            alert('select the employee picture');
+        }
+
+    }
+
+  handleClick = (e) => {
+    if(e.target.classList){
+        if (e.target.classList.contains('backdrop')) {
+        this.props.toggleEmployeeModal();
+        }
+    }
+  }
+
+  handleUploadFileChange = (e) => {
+     let fileSelected = e.target.files[0];
+     if(fileSelected && this.fileTypes.includes(fileSelected.type)){
+         this.setState({
+             fileURL:URL.createObjectURL(e.target.files[0])
+         })
+         
+         
+         this.setState({file:fileSelected});
+         this.setState({fileError:''});
+     } 
+     else{
+        this.setState({file:null})
+        this.setState({error:'Please select an image file(png or jpg)'})
+     }
+  }
+
+
+
+  componentDidMount() {
+  }
+
+    render(){
+        return (
+            <motion.div onClick={this.handleClick} className="backdrop" 
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            >
+                <motion.div  
+                    initial={{ y: "-100vh" }}
+                    animate={{ y: 0 }}
+                    className="modalcontainer"
+                >
+            
+                    <div className="imagecontainer">
+                        <div className="employee_image_container">
+                            { this.state.fileError && <div className="error">{ this.state.error }</div>}
+                            { this.state.file && 
+                                <img className='selectedImage' src={this.state.fileURL} />
+                                }
+                        </div>
+                        {!this.state.file &&  
+                            <form>
+                                <label>
+                                    <input className="fileuploadinput" type="file" onChange={this.handleUploadFileChange} />
+                                    <h1>pick profile</h1>
+                                </label>
+                            </form>
+                        }
+                        <CForm>
+                        <h1>Employee</h1>
+                        <p className="text-muted">Add new employee</p>
+                        <CInputGroup className="mb-3">
+                            <CInputGroupPrepend>
+                            <CInputGroupText>
+                                {/* <CIcon name="cil-user" /> */}
+                            </CInputGroupText>
+                            </CInputGroupPrepend>
+                            <CInput 
+                                onChange={ e=> {this.setState({employeeUsername:e.target.value})}} 
+                                type="text" placeholder="Username" autoComplete="username" />
+                        </CInputGroup>
+                        <CInputGroup className="mb-3">
+                            <CInputGroupPrepend>
+                            <CInputGroupText>
+                                {/* <CIcon name="cil-lock-locked" /> */}
+                            </CInputGroupText>
+                            </CInputGroupPrepend>
+                            <CInput 
+                                onChange={ e=> {this.setState({employeePassword:e.target.value})}} 
+                                type="password" placeholder="Password" autoComplete="new-password" />
+                        </CInputGroup>
+                        <CButton 
+                            onClick={this._save} 
+                            color="primary" block>ADD</CButton>
+                        </CForm>
+
+                    </div>
+                </motion.div>
+            </motion.div>
+        )
+    }
+}
