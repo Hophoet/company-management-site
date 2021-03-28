f49ba255903e5d0fdb4fca75c4b16ed2da9d1129
hophoet <hophoet@gmail.com>
2021-03-28 16:10:32 +0000

    (api) - add register api function for the users registration

--- src/api/authentication.js
+++ src/api/authentication.js
@@ -1,4 +1,4 @@
-import {domain, login_url} from './setup';
+import {domain, login_url, register_url} from './setup';
 
 function login(username, password){
    return new Promise( (resolve, reject) => {
@@ -31,6 +31,40 @@ function login(username, password){
 
    })
 }
+
+
+function register(username, password){
+   return new Promise( (resolve, reject) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "sessionid=bc2iakrk8931d7vuemt4zokzh8mtwy7clq; csrftoken=nljayGbkE6SoP9XjdkfPwvIRDWGrfJbypkiOZnWsJs8VznBw9EuN6UCr18pzOXh4mDq9");
+    
+    var formdata = new FormData();
+    formdata.append("username", username);
+    formdata.append("password1", password);
+    formdata.append("password2", password);
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: formdata,
+      redirect: 'follow'
+    };
+    fetch(register_url, requestOptions)
+        .then(response => {
+            let status_code = response.status;
+            console.log(response)
+            return response.json();
+        })
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {
+            reject(error);
+        })
+
+   })
+}
+
 
 
 export {
