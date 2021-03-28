67f1c49b07e403d725d8da1d316f6f873f88f3b4
hophoet <hophoet@gmail.com>
2021-03-28 16:09:52 +0000

    (api) - set user registration endpoint url

--- src/api/setup.js
+++ src/api/setup.js
@@ -1,7 +1,9 @@
 
 const domain = 'http://localhost:8000';
-const login_url = `${domain}/rest-auth/login/`
+const login_url = `${domain}/rest-auth/login`
+const register_url = `${domain}/rest-auth/registration`
 export {
     domain,
-    login_url
+    login_url,
+    register_url
 }
