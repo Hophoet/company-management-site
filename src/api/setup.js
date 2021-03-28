345fe2367bb4dce204cf298c1d03d6f133c0f69b
hophoet <hophoet@gmail.com>
2021-03-28 16:28:03 +0000

    (api) - set employees adding url

--- src/api/setup.js
+++ src/api/setup.js
@@ -3,9 +3,11 @@ const domain = 'http://localhost:8000';
 const login_url = `${domain}/rest-auth/login`
 const register_url = `${domain}/rest-auth/registration`
 const get_employees_url = `${domain}/api/employees`
+const add_employee_url = `${domain}/api/employees/add`
 export {
     domain,
     login_url,
     register_url,
-    get_employees_url
+    get_employees_url,
+    add_employee_url
 }
