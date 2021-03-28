ac9183d7e0b973dbba3f4793c53a01033e66e1df
hophoet <hophoet@gmail.com>
2021-03-28 16:38:16 +0000

    (api) - add deleteEmployee for the employees deletion

--- src/api/employee.js
+++ src/api/employee.js
@@ -1,4 +1,5 @@
-import { get_employees_url, add_employee_url, update_employee_url } from './setup'
+import { get_employees_url,  delete_employee_url,
+         add_employee_url, update_employee_url } from './setup'
 
 
 function getEmployees(authToken){
@@ -85,6 +86,40 @@ function updateEmployee(authToken, employee_id, username, salary, pictureName){
 
     let status_code = 0;
     fetch(update_employee_url, requestOptions)
+        .then(response => {
+            status_code = response.status;
+            return response.json();
+        })
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {
+            reject(error);
+        })
+
+   })
+}
+
+
+
+function deleteEmployee(authToken, employee_id){
+   return new Promise( (resolve, reject) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "sessionid=bc2iakrk8931d7vuemt4zokzh8mtwy7clq; csrftoken=nljayGbkE6SoP9XjdkfPwvIRDWGrfJbypkiOZnWsJs8VznBw9EuN6UCr18pzOXh4mDq9");
+    myHeaders.append("Authorization", "Token 	"+authToken); 
+
+    var formdata = new FormData();
+    formdata.append("employee_id", employee_id);
+
+    var requestOptions = {
+      method: 'DELETE',
+      headers: myHeaders,
+      body:formdata,
+      redirect: 'follow'
+    };
+
+    let status_code = 0;
+    fetch(delete_employee_url, requestOptions)
         .then(response => {
             status_code = response.status;
             return response.json();
