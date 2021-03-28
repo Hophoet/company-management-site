8fade9782caf823032486123968bfc660bbbb3ad
hophoet <hophoet@gmail.com>
2021-03-28 16:39:44 +0000

    (refactory) - comments

--- src/api/setup.js
+++ src/api/setup.js
@@ -1,11 +1,16 @@
 
 const domain = 'http://localhost:8000';
+// Authentication
 const login_url = `${domain}/rest-auth/login`
 const register_url = `${domain}/rest-auth/registration`
+// Employees management
 const get_employees_url = `${domain}/api/employees`
 const add_employee_url = `${domain}/api/employees/add`
 const update_employee_url = `${domain}/api/employees/update`
 const delete_employee_url = `${domain}/api/employees/delete`
+
+
+
 export {
     domain,
     login_url,
