8c9599f1800db2d52187230926005e18ba1b5575
hophoet <hophoet@gmail.com>
2021-03-28 16:29:14 +0000

    (api) - add addEmployee for the employee adding request handle

--- src/api/employee.js
+++ src/api/employee.js
@@ -1,4 +1,4 @@
-import { get_employees_url } from './setup'
+import { get_employees_url, add_employee_url } from './setup'
 
 
 function getEmployees(authToken){
@@ -16,6 +16,40 @@ function getEmployees(authToken){
         .then(response => {
             status_code = response.status;
             console.log(response)
+            return response.json();
+        })
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {
+            reject(error);
+        })
+
+   })
+}
+
+function addEmployee(authToken, username, password, salary, pictureName){
+   return new Promise( (resolve, reject) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "sessionid=bc2iakrk8931d7vuemt4zokzh8mtwy7clq; csrftoken=nljayGbkE6SoP9XjdkfPwvIRDWGrfJbypkiOZnWsJs8VznBw9EuN6UCr18pzOXh4mDq9");
+    myHeaders.append("Authorization", "Token 	"+authToken); 
+
+    var formdata = new FormData();
+    formdata.append("username", username);
+    formdata.append("salary", salary);
+    formdata.append("picture", fileInput.files[0], pictureName);
+    formdata.append("password", password);
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body:formdata,
+      redirect: 'follow'
+    };
+    let status_code = 0;
+    fetch(add_employee_url, requestOptions)
+        .then(response => {
+            status_code = response.status;
             return response.json();
         })
         .then(response => {
