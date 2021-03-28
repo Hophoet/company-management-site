e99672c6d0d3f05847a8a8cc552dbdd5daf15bad
hophoet <hophoet@gmail.com>
2021-03-28 17:02:17 +0000

    (api) - set tasks deletion request endpoint

--- src/api/setup.js
+++ src/api/setup.js
@@ -11,6 +11,7 @@ const delete_employee_url = `${domain}/api/employees/delete`
 // Tasks management
 const add_task_url = `${domain}/api/tasks/add`
 const update_task_url = `${domain}/api/tasks/update`
+const delete_task_url = `${domain}/api/tasks/delete`
 
 
 export {
@@ -22,5 +23,6 @@ export {
     update_employee_url,
     delete_employee_url,
     add_task_url,
-    update_task_url
+    update_task_url,
+    delete_task_url
 }
