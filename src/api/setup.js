66448ed002001c449e7eebfba5e62f14e7b80c75
hophoet <hophoet@gmail.com>
2021-03-28 16:33:41 +0000

    (api) - set employees update request url

--- src/api/setup.js
+++ src/api/setup.js
@@ -4,10 +4,12 @@ const login_url = `${domain}/rest-auth/login`
 const register_url = `${domain}/rest-auth/registration`
 const get_employees_url = `${domain}/api/employees`
 const add_employee_url = `${domain}/api/employees/add`
+const update_employee_url = `${domain}/api/employees/update`
 export {
     domain,
     login_url,
     register_url,
     get_employees_url,
-    add_employee_url
+    add_employee_url,
+    update_employee_url
 }
