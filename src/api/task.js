7259a4b71eecd5fda6e19b0ba240ef498c1083ab
hophoet <hophoet@gmail.com>
2021-03-28 16:49:26 +0000

    (api) - add addTask api function for tasks adding request handle

---
+++ src/api/task.js
@@ -0,0 +1,35 @@
+import { add_task_url } from './setup';
+
+function addTask(authToken, employee_id, title, description, deadline){
+   return new Promise( (resolve, reject) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "sessionid=bc2iakrk8931d7vuemt4zokzh8mtwy7clq; csrftoken=nljayGbkE6SoP9XjdkfPwvIRDWGrfJbypkiOZnWsJs8VznBw9EuN6UCr18pzOXh4mDq9");
+    myHeaders.append("Authorization", "Token 	"+authToken); 
+
+    var formdata = new FormData();
+    formdata.append("title", title);
+    formdata.append("description", description);
+    formdata.append("employee_id", employee_id);
+    formdata.append("deadline", deadline);
+
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body:formdata,
+      redirect: 'follow'
+    };
+    fetch(add_employee_url, requestOptions)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {
+            reject(error);
+        })
+
+   })
+}
+
+
