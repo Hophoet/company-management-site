d5daceea3b8751ba4f799b12cf7e52ccf004a6e5
hophoet <hophoet@gmail.com>
2021-03-28 17:03:02 +0000

    (api) - add deleteTask api function for tasks deletion

--- src/api/task.js
+++ src/api/task.js
@@ -1,4 +1,4 @@
-import { add_task_url, update_task_url } from './setup';
+import { add_task_url, update_task_url, delete_task_url } from './setup';
 
 function addTask(authToken, employee_id, title, description, deadline){
    return new Promise( (resolve, reject) => {
@@ -18,7 +18,7 @@ function addTask(authToken, employee_id, title, description, deadline){
       body:formdata,
       redirect: 'follow'
     };
-    fetch(add_employee_url, requestOptions)
+    fetch(add_task_url, requestOptions)
         .then(response => {
             return response.json();
         })
@@ -70,3 +70,32 @@ function updateTask(authToken, task_id, title, description, deadline){
 
 
 
+
+function deleteTask(authToken, task_id){
+   return new Promise( (resolve, reject) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Cookie", "sessionid=bc2iakrk8931d7vuemt4zokzh8mtwy7clq; csrftoken=nljayGbkE6SoP9XjdkfPwvIRDWGrfJbypkiOZnWsJs8VznBw9EuN6UCr18pzOXh4mDq9");
+    myHeaders.append("Authorization", "Token 	"+authToken); 
+
+    var formdata = new FormData();
+    formdata.append("task_id", task_id);
+
+    var requestOptions = {
+      method: 'DELETE',
+      headers: myHeaders,
+      body:formdata,
+      redirect: 'follow'
+    };
+
+    fetch(delete_task_url, requestOptions)
+        .then(response => {
+            return response.json();
+        })
+        .then(response => {
+            resolve(response);
+        })
+        .catch(error => {
+            reject(error);
+        })
+   })
+}
