ff0dc1546d5a6cd82c92275f0bf3af0874f991c5
hophoet <hophoet@gmail.com>
2021-03-28 16:37:32 +0000

    (api) - set employees deletion request endpoint url

--- src/api/setup.js
+++ src/api/setup.js
@@ -5,11 +5,13 @@ const register_url = `${domain}/rest-auth/registration`
 const get_employees_url = `${domain}/api/employees`
 const add_employee_url = `${domain}/api/employees/add`
 const update_employee_url = `${domain}/api/employees/update`
+const delete_employee_url = `${domain}/api/employees/delete`
 export {
     domain,
     login_url,
     register_url,
     get_employees_url,
     add_employee_url,
-    update_employee_url
+    update_employee_url,
+    delete_employee_url
 }
