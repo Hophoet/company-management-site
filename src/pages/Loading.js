8a30974ac17e16a19f00b0c8e3420d2bec5c5d94
hophoet <hophoet@gmail.com>
2021-03-29 19:15:38 +0000

    (methods) - update _handleRoute function

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,15 +1,23 @@
 import React, {useState, useEffect} from 'react'
 import {Link, useHistory} from 'react-router-dom'
-
+import { useSelector, useDispatch } from 'react-redux';
 
 const Loading = () => {
+    const user = useSelector(state => {
+       return  state.auth.user;
+    });
     const history = useHistory();
 	useEffect(() => {
         _handleRoute();
     }, [])	
 
     const _handleRoute = () => {
-        history.push('/login')
+        if(user.authToken){
+            history.push('/admin')
+        }
+        else{
+            history.push('/login')
+        }
     }
 
     return (
