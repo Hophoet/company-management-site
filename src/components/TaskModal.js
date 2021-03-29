82f35cd13cbb3c34e4889a0a423627bec000cdc3
hophoet <hophoet@gmail.com>
2021-03-29 15:15:04 +0000

    implement the tasks add and update

--- src/components/TaskModal.js
+++ src/components/TaskModal.js
@@ -9,13 +9,15 @@ import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 
+import { addTask, updateTask } from '../api/task';
+
 import {
   CButton,
   CForm,
   CInput,
   CInputGroup,
   CInputGroupPrepend,
-  CInputGroupText,
+  CInputGroupText
 } from '@coreui/react'
 
 
@@ -27,37 +29,76 @@ export default class AddPictureModal extends React.Component{
             taskTitle:(this.props.taskToUpdate && this.props.taskToUpdate.title)?this.props.taskToUpdate.title:'',
             taskDeadline:( this.props.taskToUpdate && this.props.taskToUpdate.deadline)?this.props.taskToUpdate.deadline:'',
             taskDescription:( this.props.taskToUpdate && this.props.taskToUpdate.description)?this.props.taskToUpdate.description:'',
-            taskEmployee:( this.props.taskToUpdate && this.props.taskToUpdate.employee)?this.props.taskToUpdate.employee:''
+            taskEmployee:( this.props.taskToUpdate && this.props.taskToUpdate.employee)?this.props.taskToUpdate.employee:'',
        }
         this.taskToUpdate = this.props.taskToUpdate
+        this.employees = this.props.employees;
+        this.authToken = this.props.authToken;
     }
 
     _save = () => {
-        if(this._formIsValid()){
-            //check if it the update
-            if(this.taskToUpdate){
-                this._updateTask()
-            }
-            else{
-                this._addNewTask()
+        if(this.taskToUpdate){
+            if(this._updateTaskFormIsValid()){
+                if(this.taskToUpdate){
+                    this._updateTask()
+                }
             }
         }
+        else{
+            if(this._addTaskFormIsValid()){
+                //check if it the update
+                if(!this.taskToUpdate){
+                    this._addNewTask()
+                }
+            }
+        }
+
 
     }
     
     _addNewTask = () => {
-        alert('ready to add the new employee')
+        addTask(this.authToken, 
+                this.state.selectedEmployee, 
+                this.state.taskTitle, 
+                this.state.taskDescription,
+                this.state.taskDeadline
+        )
+        .then(response => {
+            if(response.title){
+                this.props.getTasks();
+                this.props.toggleTaskModal();
+            }
+            console.log('add successfulyy')
+        })
+        .catch(error => {
+            console.log('add failed')
+            console.log(error)
+
+        })
     }
 
     _updateTask = () => {
-        alert('ready to update the task')
+        updateTask(
+            this.authToken, 
+            this.taskToUpdate.id,
+            this.state.taskTitle,
+            this.state.taskDescription,
+            this.state.taskDeadline
+        )
+        .then(response => {
+            this.props.getTasks();
+            this.props.toggleTaskModal();
+        })
+        .catch(error => {
+            console.log(error);
+        })
     }
 
     
 
 
-    _formIsValid = () => {
-        if(this.state.taskTitle && this.state.taskDeadline && this.state.taskDescription && this.state.taskEmployee){
+    _addTaskFormIsValid = () => {
+        if(this.state.taskTitle && this.state.taskDeadline && this.state.taskDescription && this.state.selectedEmployee){
             return true
         }
         else if(!this.state.taskTitle){
@@ -66,8 +107,24 @@ export default class AddPictureModal extends React.Component{
         else if(!this.state.taskDescription){
             alert('Enter the task description');
         }
-        else if(!this.state.taskEmployee){
+        else if(!this.state.selectedEmployee){
             alert('select the task employee');
+        }
+        else if(!this.state.taskDealine){
+            alert('select the task dealine');
+        }
+
+    }
+    _updateTaskFormIsValid = () => {
+        if(this.state.taskTitle && this.state.taskDeadline && 
+            this.state.taskDescription){
+            return true
+        }
+        else if(!this.state.taskTitle){
+            alert('Enter the task title');
+        }
+        else if(!this.state.taskDescription){
+            alert('Enter the task description');
         }
         else if(!this.state.taskDealine){
             alert('select the task dealine');
@@ -88,12 +145,10 @@ export default class AddPictureModal extends React.Component{
 
   }
     selectedEmployeeChange = (event) => {
-        console.log(event.target.value);
         this.setState({selectedEmployee:event.target.value});
     }
 
     render(){
-        const employees = [{id:'kdj34', name:'ali'}, {id:"lsdfj43", name:"gray"}]
         return (
             <motion.div onClick={this.handleClick} className="backdrop" 
             initial={{ opacity: 0 }}
@@ -139,21 +194,10 @@ export default class AddPictureModal extends React.Component{
                             </CInputGroupPrepend>
                             <CInput 
                                 value={this.state.taskDeadline}
-                                onChange={ e=> {this.setState({taskDeadline:e.target.value})}} 
-                                type="text" placeholder="Deadline" />
+                                onChange={ e=> {this.setState({taskDeadline:e.target.value + " 00:00:00"})}} 
+                                type="date" placeholder="Deadline" />
                         </CInputGroup>
-                        <CInputGroup className="mb-3">
-                            <CInputGroupPrepend>
-                            <CInputGroupText>
-                                {/* <CIcon name="cil-lock-locked" /> */}
-                            </CInputGroupText>
-                            </CInputGroupPrepend>
-                            <CInput 
-                                value={this.state.taskEmployee}
-                                onChange={ e=> {this.setState({taskEmployee:e.target.value})}} 
-                                type="text" placeholder="Employee" autoComplete="new-password" />
-                        </CInputGroup>
-
+                    {  !this.taskToUpdate &&
                     <FormControl className="employeeSelectForm">
                         <InputLabel id="demo-simple-select-label">employee</InputLabel>
                         <Select
@@ -162,12 +206,13 @@ export default class AddPictureModal extends React.Component{
                         value={this.state.selectedEmployee}
                         onChange={this.selectedEmployeeChange}
                         >
-                        {employees && employees.map(employee => (
-                            <MenuItem name={employee.name} value={employee.id}>{employee.name}</MenuItem>
+                        {this.employees && this.employees.map(employee => (
+                            <MenuItem name={employee.username} value={employee.id}>{employee.username}</MenuItem>
                         ))
                         }
                         </Select>
                     </FormControl>
+                    }  
 
                         <CButton 
                             onClick={this._save} 
