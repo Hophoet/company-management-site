40f65864452b9446732ec1d4f5419e4eb53ec8e4
hophoet <hophoet@gmail.com>
2021-03-29 23:50:10 +0000

    (methods) - add _isAdmin to make a dynamic navigation

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {Link, useHistory} from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
+import {isAdmin } from '../api/authentication'
 
 const Loading = () => {
     const user = useSelector(state => {
@@ -13,11 +14,27 @@ const Loading = () => {
 
     const _handleRoute = () => {
         if(user.authToken){
-            history.push('/admin')
+            _isAdmin(user.authToken)
         }
         else{
             history.push('/login')
         }
+    }
+    
+    const _isAdmin = (authToken) =>{
+        isAdmin(authToken)
+        .then(response =>{
+            if(response.is_admin){
+                history.push('/admin')
+            }
+            else{
+                history.push('/employee')
+            }
+        })
+        .catch(error => {
+            console.log('is admin error')
+            console.log(error)
+        })
     }
 
     return (
