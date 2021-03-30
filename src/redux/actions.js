73225157745eae0fc3e59bb717dd33f477948b47
hophoet <hophoet@gmail.com>
2021-03-30 12:24:50 +0000

    (redux) - add SET_AUTH_TOKEN action, create setAuthToken to set the user authentication token, setIsAdmin to set authenticated user role

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,8 +1,23 @@
 export const SET_USER = 'SET_USER';
 export const SET_IS_ADMIN = 'SET_IS_ADMIN'
+export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN'
 export function setUser(user){
     return {
         type:SET_USER,
         user
+    }
+}
+
+export function setAuthToken(authToken){
+    return {
+        type:SET_AUTH_TOKEN,
+        authToken
+    }
+}
+
+export function setIsAdmin(isAdmin){
+    return {
+        type:SET_IS_ADMIN,
+        isAdmin
     }
 }
