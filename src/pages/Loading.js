cb2060c51d2ebb030c8c00ba3f496dd720dbbb81
hophoet <hophoet@gmail.com>
2021-03-30 12:32:11 +0000

    (methods, updates) - add _getAuthenticatedUser base on api function getAuthenticatedUser to get and store the authenticated user into the reux store, update because of the set of the authToken redux store, the user authentication token will now be set to the redux authToken store object instead of the user store

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,41 +1,70 @@
 import React, {useState, useEffect} from 'react'
 import {Link, useHistory} from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
-import {isAdmin } from '../api/authentication'
+import {isAdmin, getAuthenticatedUser  } from '../api/authentication'
+import  {setUser } from '../redux/actions'
 
 const Loading = () => {
+    const authToken = useSelector(state => {
+       return  state.auth.authToken;
+    });
     const user = useSelector(state => {
        return  state.auth.user;
     });
+
+    const dispatch = useDispatch();
     const history = useHistory();
 	useEffect(() => {
+        console.log('loadind auth token')
+        console.log(authToken)
+        console.log('loading user')
+        console.log(user);
         _handleRoute();
     }, [])	
 
     const _handleRoute = () => {
-        if(user.authToken){
-            _isAdmin(user.authToken)
+        if(authToken){
+            _getAuthenticatedUser(authToken);
         }
         else{
-            history.push('/login')
+            history.push('/login');
+            console.log('not auth token')
         }
     }
     
+
     const _isAdmin = (authToken) =>{
         isAdmin(authToken)
         .then(response =>{
             if(response.is_admin){
-                history.push('/admin')
+                //history.push('/admin');
             }
             else{
-                history.push('/employee')
+                //history.push('/employee');
             }
         })
         .catch(error => {
-            console.log('is admin error')
-            console.log(error)
+            console.log('is admin error');
+            console.log(error);
         })
     }
+
+    const _getAuthenticatedUser = (authToken) => {
+        getAuthenticatedUser(authToken)
+        .then(response => {
+            dispatch(setUser(response))
+            if(response.is_staff){
+                history.push('/admin');
+            }
+            else{
+                history.push('/employee');
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+
 
     return (
     <div className="pt-3 text-center">
