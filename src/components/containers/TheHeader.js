3446fffe6aaf1c981b78060763b64585f6383364
hophoet <hophoet@gmail.com>
2021-03-30 12:37:57 +0000

    (methods, components)  render the authenticated user username get from props and manage it logout with _logout function

--- src/components/containers/TheHeader.js
+++ src/components/containers/TheHeader.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import {
   CHeader,
   CToggler,
@@ -7,37 +7,52 @@ import {
   CHeaderNavItem,
   CHeaderNavLink,
   CSubheader,
-  CBreadcrumbRouter,
+  CBreadcrumbRouter
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
+import { useSelector, useDispatch } from 'react-redux';
+import {useHistory} from 'react-router-dom'
+import  {setUser, setAuthToken } from '../../redux/actions'
 
+const TheHeader = ({user}) => {
+    const history = useHistory();
+    const dispatch = useDispatch();
 
-const TheHeader = () => {
+    const _logout = () => {
+        if(window.confirm('Do you want to logout ?')){
+            dispatch(setUser({}));
+            dispatch(setUser(null));
+            history.push('/login');
+        }
+    }
 
+    
 
   return (
     <CHeader withSubheader>
-      <CToggler
-        inHeader
-        className="ml-md-3 d-lg-none"
-        onClick={() => {}}
-      />
-      <CToggler
-        inHeader
-        className="ml-3 d-md-down-none"
-        onClick={() => {}}
-      />
+          {/* <CToggler
+            inHeader
+            className="ml-md-3 d-lg-none"
+            onClick={() => {}}
+          />
+          <CToggler
+            inHeader
+            className="ml-3 d-md-down-none"
+            onClick={() => {}}
+          /> */}
       <CHeaderBrand className="mx-auto d-lg-none" to="/">
         <CIcon name="logo" height="48" alt="Logo"/>
       </CHeaderBrand>
 
       <CHeaderNav className="d-md-down-none mr-auto">
-        <CHeaderNavItem  className="px-3">
-          <CHeaderNavLink to="/users">Admin name</CHeaderNavLink>
-        </CHeaderNavItem>
       </CHeaderNav>
 
       <CHeaderNav className="px-3">
+
+        <CHeaderNavItem  className="px-3">
+          <span>{user && user.is_staff?'Administrator':'Employee'}  {user && user.username?user.username:''} </span>
+          <button onClick={_logout} className='btn btn-danger' >logout</button>
+        </CHeaderNavItem>
       </CHeaderNav>
 
       <CSubheader className="px-3 justify-content-between">
